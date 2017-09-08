751f8144eda13c0efe1cce1bf6728f26d72ab3c6
kifran72 <cr91700@gmail.com>
2017-09-08 06:24:50 +0200

    finnal

--- interface.sh
+++ interface.sh
@@ -4,16 +4,32 @@
 sudo add-apt-repository ppa:noobslab/icons
 sudo apt-get update
 sudo apt-get install ultra-flat-icons
+echo 'Y';
 
 #Installation du theme Arc-flatabulous
 sudo apt install autoconf
+echo 'Y';
 sudo apt install automake
+echo 'Y';
 sudo apt install pkg-config
+echo 'Y';
 sudo apt install libgtk-3-dev
+echo 'Y';
 sudo apt install git
+echo 'Y';
 sudo git clone https://github.com/andreisergiu98/arc-flatabulous-theme && cd arc-flatabulous-theme
 sudo ./autogen.sh --prefix=/usr
 sudo make install
 
+#ZSH
+sudo apt install zsh
+echo 'Y';
+sudo apt install curl
+echo 'Y';
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
 #docky
 sudo apt install docky
+echo 'Y';
